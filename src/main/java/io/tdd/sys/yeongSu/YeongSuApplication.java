e525556c289de0f2548c9403e7d6013c5b3827ea
sys970717 <sys970717@naver.com>
2022-05-03 14:57:35 +0900

    환경별 config세팅

--- src/main/java/io/tdd/sys/yeongSu/YeongSuApplication.java
+++ src/main/java/io/tdd/sys/yeongSu/YeongSuApplication.java
@@ -1,13 +1,22 @@
 package io.tdd.sys.yeongSu;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class YeongSuApplication {
+	@Value("${spring.profiles.active}")
+	private String profile;
 
 	public static void main(String[] args) {
 		SpringApplication.run(YeongSuApplication.class, args);
 	}
 
+	@PostConstruct
+	private void start() {
+		System.out.println("profile = "+profile);
+	}
 }
